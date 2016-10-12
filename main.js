5e2db6be2362e34b916abb559fa94fdc44d39473
Jack Storrs <jackstorrs8@gmail.com>
2016-10-12 19:04:51 -0400

    content is now opening and closing the way we want it to, still need to add transition effects and have the 'click' function close other boxes when we open the current.

--- main.js
+++ main.js
@@ -14,12 +14,15 @@
 function changeHeight (event) {
   var target = $(event.target);
   var currentBox;
+  var previousBox;
   // console.log(target);
-    if (target[0].id === "h1") { currentBox = $("#content1")};
+    if (target[0].id === "h1") {currentBox = $("#content1")};
     if (target[0].id === "h2") { currentBox = $("#content2")};
     if (target[0].id === "h3") { currentBox = $("#content3")};
     currentBox.toggleClass("bigBox");
-  console.log(currentBox)
+    currentBox.toggleClass("boxcontent");
+    // currentBox[0].innerHTML = "";
+    console.log(currentBox);
 
 };
 

aeace19fdec2569a54806954932ecdf9ce9a1ba2
Jack Storrs <jackstorrs8@gmail.com>
2016-10-12 19:31:48 -0400

    started playing with currentBox, lastBox, nextBox, commiting before i destroy my functional code.

--- main.js
+++ main.js
@@ -1,27 +1,24 @@
-
-
-
-
-
-
-
-
-
-
-
+var firstbox = $("#content1");
+console.log(firstbox);
+firstbox[0].className = "bigBox";
 
 
 function changeHeight (event) {
   var target = $(event.target);
   var currentBox;
-  var previousBox;
+  var lastBox;
+  var nextBox;
   // console.log(target);
     if (target[0].id === "h1") {currentBox = $("#content1")};
-    if (target[0].id === "h2") { currentBox = $("#content2")};
+    if (target[0].id === "h2") {
+      currentBox = $("#content2");
+      lastBox = $("#content1")};
+      nextBox = $("#content3");
     if (target[0].id === "h3") { currentBox = $("#content3")};
     currentBox.toggleClass("bigBox");
+    lastBox.toggleClass("boxcontent");
+    nextBox.toggleClass("boxcontent");
     currentBox.toggleClass("boxcontent");
-    // currentBox[0].innerHTML = "";
     console.log(currentBox);
 
 };

881f66ab81cc3590fc04693aff74d323e96b5267
Jack Storrs <jackstorrs8@gmail.com>
2016-10-12 17:19:55 -0400

    Got color changer function up and running for the headers, starting to write out the function for our box-height transitions.

---
+++ main.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+function changeHeight (event) {
+  var target = $(event.target);
+  var currentBox;
+  // console.log(target);
+    if (target[0].id === "h1") { currentBox = $("#content1")};
+    if (target[0].id === "h2") { currentBox = $("#content2"};
+    if (target[0].id === "h3") { currentBox = $("#content3"};
+  console.log(currentBox)
+
+};
+
+function changeColor (event) {
+  var target = $(event.target);
+  target.toggleClass("headHover");
+};
+
+
+var head = $("header");
+head.click(changeHeight);
+head.hover(changeColor);
